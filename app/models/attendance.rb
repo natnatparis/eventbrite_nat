bbdb8d0ab7906f734995c2beba6527eefdc3e7bc
natnatparis <natnatparis1@gmail.com>
2019-08-16 00:59:45 +0200

    debug

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,9 +2,10 @@ class Attendance < ApplicationRecord
 
     belongs_to :event
     belongs_to :user
-    after_create : new_attendee_send
+    after_create :new_attendee_send
 
     def new_attendee_send
         AttendanceMailer.new_attendee_email(self).deliver_now
-      end
+    end
+    end
 end
