8f1a065457f58ccfb707a502e45e4c1a1d0373dc
natnatparis <natnatparis1@gmail.com>
2019-08-16 11:35:03 +0200

    validation user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+   # Validations
+    validates :first_name, presence: true  
+    validates :last_name, presence: true 
+    validates :uid, uniqueness: true
+
     has_many :attendances
     has_many :events, through: :attendances
     has_many :event_admin_id, foreign_key: 'administrator_id' , class_name: "Event"
