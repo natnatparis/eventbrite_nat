4e03312c23a3d9a78b66625f52a4543f2201ee97
natnatparis <natnatparis1@gmail.com>
2019-08-17 00:52:24 +0200

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
-include Faker
+#require 'faker'
+#include Faker
 
-20.times do 
-User.create!( i = rand(1..100)
-  first_name: Faker::Name.first_name
-  last_name: Faker::Name.last_name
-  description: Faker::ChuckNorris.description
-  pwd = Faker::Internet.password
-    email: "#{faker_first_name.downcase}"+"#{i}"+"@yopmail.com",
-    password: pwd, password_confirmation: pwd)
-end
-puts "20 utilisateurs ont été générés"
+#20.times do 
+#User.create!( i = rand(1..100)
+
+#  first_name: Faker::Name.first_name
+#  last_name: Faker::Name.last_name
+#  description: Faker::ChuckNorris.fact
+#  pwd = Faker::Internet.password
+#    email: "#{faker_first_name.downcase}"+"#{i}"+"@yopmail.com",
+#    password: pwd, password_confirmation: pwd)
+#end
+#puts "20 utilisateurs ont été générés"
 
 #30.times do 
  # event = Event.create!(start_date: Faker::Date.forward(days: 365), duration: rand(1..20) * 5, title: Faker::Book.title, description: "Cet événement attend de recevoir une description.", price: rand(1..100) * 10, location: Faker::Address.city, event_planner_id: User.find(rand(1..20)).id )
