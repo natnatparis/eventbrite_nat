af888dae3d38e7202d4804b85c6f55d67df48524
natnatparis <natnatparis1@gmail.com>
2019-08-16 18:53:30 +0200

    add user views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
+  #devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   #root to: 'home#index' 
   resources :events
   resources :attendances
